dd968d30b5f578159c94359874033f6642ce5040
King of MC <78730822+King-of-MC@users.noreply.github.com>
2021-10-07 16:29:39 +0530

    working version

---
+++ main.js
@@ -0,0 +1,37 @@
+var canvas=document.getElementById("mycanvas");
+var ctx=canvas.getContext("2d");
+var colororange="orange"
+var colorblue="darkblue"
+var colorblack="black"
+var colorgreen="forestgreen"
+var colorred="red"
+// ANOTHER GAP 
+ctx.beginPath();
+ctx.strokeStyle=colororange;
+ctx.lineWidth=6;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+// Gaps Everywhere 
+ctx.beginPath();
+ctx.strokeStyle=colorblue;
+ctx.lineWidth=6;
+ctx.arc(160,160,40,0,2*Math.PI);
+ctx.stroke();
+// The Gaps Win the olympics gaps are happy 
+ctx.beginPath();
+ctx.strokeStyle=colorblack;
+ctx.lineWidth=6;
+ctx.arc(250,160,40,0,2*Math.PI);
+ctx.stroke();
+// we congragulagte gaps for helping us 
+ctx.beginPath();
+ctx.strokeStyle=colorgreen;
+ctx.lineWidth=6;
+ctx.arc(300,200,40,0,2*Math.PI);
+ctx.stroke();
+// HOW MUCH MORE // THINGs LITERALLY 
+ctx.beginPath();
+ctx.strokeStyle=colorred;
+ctx.lineWidth=6;
+ctx.arc(340,160,40,0,2*Math.PI);
+ctx.stroke();
